8451b8cff09c0a78360d044268aaaef0e242df04
Abdul-Basith-siddiqui <mohdabdulbasith17@gmail.com>
2025-04-03 17:19:29 +0530

    isssue open

--- auth.js
+++ auth.js
@@ -1,4 +1,30 @@
-// auth.js
+// // auth.js
+// const jwt = require('jsonwebtoken');
+ 
+// const SECRET = 'your-256-bit-secret-password-password';
+ 
+// function authenticateToken(req, res, next) {
+//   const authHeader = req.headers['authorization'];
+//   const token = authHeader?.split(' ')[1];
+ 
+//   if (!token) return res.sendStatus(401); // Unauthorized
+ 
+//      jwt.verify(token, SECRET, {
+//     issuer: 'myapi.example.com',
+//     audience: 'myapi-client' 
+//   }, (err, user) => {
+//     if (err) return res.sendStatus(403); // Forbidden
+//     req.user = user;
+//     next();
+//   });
+  
+// }
+ 
+// module.exports = { authenticateToken, SECRET };
+
+
+
+
 const jwt = require('jsonwebtoken');
  
 const SECRET = 'your-256-bit-secret-password-password';
@@ -9,15 +35,11 @@ function authenticateToken(req, res, next) {
  
   if (!token) return res.sendStatus(401); // Unauthorized
  
-     jwt.verify(token, SECRET, {
-    issuer: 'myapi.example.com',
-    audience: 'myapi-client' 
-  }, (err, user) => {
+  jwt.verify(token, SECRET, (err, user) => {
     if (err) return res.sendStatus(403); // Forbidden
     req.user = user;
     next();
   });
-  
 }
  
 module.exports = { authenticateToken, SECRET };
