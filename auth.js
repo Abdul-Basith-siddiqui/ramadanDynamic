4deeab9c55b17562dabe1a54eced805f64a10fca
Abdul-Basith-siddiqui <mohdabdulbasith17@gmail.com>
2025-04-03 17:52:32 +0530

    issuse closed

--- auth.js
+++ auth.js
@@ -1,30 +1,4 @@
-// // auth.js
-// const jwt = require('jsonwebtoken');
- 
-// const SECRET = 'your-256-bit-secret-password-password';
- 
-// function authenticateToken(req, res, next) {
-//   const authHeader = req.headers['authorization'];
-//   const token = authHeader?.split(' ')[1];
- 
-//   if (!token) return res.sendStatus(401); // Unauthorized
- 
-//      jwt.verify(token, SECRET, {
-//     issuer: 'myapi.example.com',
-//     audience: 'myapi-client' 
-//   }, (err, user) => {
-//     if (err) return res.sendStatus(403); // Forbidden
-//     req.user = user;
-//     next();
-//   });
-  
-// }
- 
-// module.exports = { authenticateToken, SECRET };
-
-
-
-
+// auth.js
 const jwt = require('jsonwebtoken');
  
 const SECRET = 'your-256-bit-secret-password-password';
@@ -35,11 +9,37 @@ function authenticateToken(req, res, next) {
  
   if (!token) return res.sendStatus(401); // Unauthorized
  
-  jwt.verify(token, SECRET, (err, user) => {
+     jwt.verify(token, SECRET, {
+    issuer: 'myapi.example.com',
+    audience: 'myapi-client' 
+  }, (err, user) => {
     if (err) return res.sendStatus(403); // Forbidden
     req.user = user;
     next();
   });
+  
 }
  
 module.exports = { authenticateToken, SECRET };
+
+
+
+
+// const jwt = require('jsonwebtoken');
+ 
+// const SECRET = 'your-256-bit-secret-password-password';
+ 
+// function authenticateToken(req, res, next) {
+//   const authHeader = req.headers['authorization'];
+//   const token = authHeader?.split(' ')[1];
+ 
+//   if (!token) return res.sendStatus(401); // Unauthorized
+ 
+//   jwt.verify(token, SECRET, (err, user) => {
+//     if (err) return res.sendStatus(403); // Forbidden
+//     req.user = user;
+//     next();
+//   });
+// }
+ 
+// module.exports = { authenticateToken, SECRET };
